a6c6af79432adf40beba07a772468d7ae8fc4173
prajwal <prajwaljagadeesh18@gmail.com>
2024-01-16 16:58:27 +0530

    first cordova project

---
+++ www/js/index.js
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    app.receivedEvent('deviceready');
+    CleverTap.notifyDeviceReady();
+
+
+
+
+    var stuff = ["bags", "shoes"];
+var profile = {
+    'Name': 'PrajwalCordova2',
+    'Identity': '000002',
+    'Email': 'prajwalcordova@gmail.com',
+    'Phone': '+9164611444',
+    'stuff': stuff
+}
+CleverTap.onUserLogin(profile);
+
+
+
+
+
+var eventData = {
+    // Key:    Value
+   'first': 'partridge',
+   'second': 'turtledoves'
+}
+CleverTap.recordEventWithName('Product Viewed', eventData);
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
